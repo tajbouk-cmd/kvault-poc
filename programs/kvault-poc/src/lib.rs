4d96b7107f22ac1be856b00d007c48e482eb5e44
yasso tsd <tajbouk@gmail.com>
2026-01-14 21:52:13 +0100

    Update lib.rs

--- programs/kvault-poc/src/lib.rs
+++ programs/kvault-poc/src/lib.rs
@@ -1,1 +1,17 @@
+use anchor_lang::prelude::*;
+
+declare_id!("9Ub23SJJRWWXRtPYEk57xwS98k73dWeXok9fpHbHjzYT");
+
+#[program]
+pub mod kvault_poc {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("KVault PoC initialized!");
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
 
